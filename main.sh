6826e7a4f1256127a6220adb9d912e411f8c4ee3
omvarikuti2-sketch <om.varikuti2@gmail.com>
2025-09-13 14:12:31 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Create necessary library symlinks FIRST
+
 sudo ln -sf /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.1.so.0 /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.0.so.37
 sudo ln -sf /usr/lib/x86_64-linux-gnu/libjavascriptcoregtk-4.1.so.0 /usr/lib/x86_64-linux-gnu/libjavascriptcoregtk-4.0.so.18
 
-# Set some colors for output messages
+
 OK="$(tput setaf 2)[OK]$(tput sgr0)"
 ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
 NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
@@ -20,12 +20,12 @@ BLUE="$(tput setaf 4)"
 SKY_BLUE="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 
-# Function to print colorful text
+
 print_color() {
     printf "%b%s%b\n" "$1" "$2" "$RESET"
 }
 
-# Display warning banner
+
 printf "\n%.0s" {1..2}
 print_color "$WARNING" "
     █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
@@ -40,23 +40,22 @@ print_color "$WARNING" "
     It will install the official AppImage version.
 "
 
-# Update and install dependencies
+
 print_color "$CAT" "Updating APT and installing required dependencies..."
 sudo apt update
 sudo apt install -y wget libfuse2 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
     libxcb-render-util0 libxcb-xinerama0 libxkbcommon-x11-0
 
-# Download Bamboo Studio AppImage
+
 print_color "$CAT" "Downloading Bamboo Studio AppImage..."
 wget https://github.com/bambulab/BambuStudio/releases/latest/download/BambuStudio-ubuntu-64bit.AppImage -O BambuStudio.AppImage
 
-# Make AppImage executable
 chmod +x BambuStudio.AppImage
 
-# Move to /usr/local/bin
+
 print_color "$CAT" "Installing Bamboo Studio to /usr/local/bin as 'bambu-studio'..."
 sudo mv BambuStudio.AppImage /usr/local/bin/bambu-studio
 
-# Launch
+
 print_color "$OK" "Installation complete. Launching Bamboo Studio..."
 bambu-studio
